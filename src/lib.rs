eef14310489e9a794c0395e7990a859b8d9e1fec
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-03-08 08:31:45 -0800

    Adding no_std

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
-
+#![no_std]
 
 mod colour_gradient;
 mod errors;
